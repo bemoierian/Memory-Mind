9ad5c7ca06a7028bbafba8685f0bf13483c4cf77
Bemoi Erian <bemoi.erian@gmail.com>
2023-06-12 23:31:39 +0300

    edited update post

--- controllers/feed.js
+++ controllers/feed.js
@@ -88,6 +88,8 @@ exports.createPost = (req, res, next) => {
         creator: req.userId
       });
       // exclude refPath from post object
+      // restPost is the post object without refPath,
+      // which is not needed in the response
       const { refPath: _, ...resPost } = post._doc;
       // ------------save post to database------------
       post
@@ -137,55 +139,49 @@ exports.getPost = (req, res, next) => {
     });
 };
 
-// exports.updatePost = (req, res, next) => {
-//   const postId = req.params.postId;
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     const error = new Error('Validation failed, entered data is incorrect.');
-//     error.statusCode = 422;
-//     throw error;
-//   }
-//   const title = req.body.title;
-//   const content = req.body.content;
-//   let imageUrl = req.body.image;
-//   if (req.file) {
-//     imageUrl = req.file.path;
-//   }
-//   if (!imageUrl) {
-//     const error = new Error('No file picked.');
-//     error.statusCode = 422;
-//     throw error;
-//   }
-//   Post.findById(postId)
-//     .then(post => {
-//       if (!post) {
-//         const error = new Error('Could not find post.');
-//         error.statusCode = 404;
-//         throw error;
-//       }
-//       if (post.creator.toString() !== req.userId) {
-//         const error = new Error('Not authorized!');
-//         error.statusCode = 403;
-//         throw error;
-//       }
-//       if (imageUrl !== post.imageUrl) {
-//         deleteImageFromLocalStorage(post.imageUrl);
-//       }
-//       post.title = title;
-//       post.imageUrl = imageUrl;
-//       post.content = content;
-//       return post.save();
-//     })
-//     .then(result => {
-//       res.status(200).json({ message: 'Post updated!', post: result });
-//     })
-//     .catch(err => {
-//       if (!err.statusCode) {
-//         err.statusCode = 500;
-//       }
-//       next(err);
-//     });
-// };
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
+      // update fields that are sent in the request
+      if (title) {
+        post.title = title;
+      }
+      if (content) {
+        post.content = content;
+      }
+
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated!', post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
--- routes/feed.js
+++ routes/feed.js
@@ -41,19 +41,21 @@ router.post(
 
 router.get('/post/:postId', isAuth, feedController.getPost);
 
-// router.put(
-//   '/post/:postId',
-//   isAuth,
-//   [
-//     body('title')
-//       .trim()
-//       .isLength({ min: 5 }),
-//     body('content')
-//       .trim()
-//       .isLength({ min: 5 })
-//   ],
-//   feedController.updatePost
-// );
+router.patch(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .optional()
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .optional()
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
 
 router.delete('/post/:postId', isAuth, feedController.deletePost);
 
